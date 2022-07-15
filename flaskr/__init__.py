a9eba160712f4e62f698a01bacb76a078a8a3571
goto <megumigoto65@gmail.com>
2022-07-15 13:25:07 +0900

    tango

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -11,7 +11,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'dsafhisdf'
-    app.config['SQLALCHEMY_DATABASE_URI'] =  'postgresql://postgres:4869@localhost:5432/tango1'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or  'postgresql://postgres:4869@localhost:5432/tango1'
     migrate = Migrate(app, db)
     db.init_app(app)
 
