30455b05d8663f1958419716da0a847d41d7745c
goto <megumigoto65@gmail.com>
2022-07-15 15:40:43 +0900

    tango

--- flaskr/models.py
+++ flaskr/models.py
@@ -3,13 +3,13 @@ from . import db
 class Word1(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     korean = db.Column(db.String())
-    meaning = db.Column(db.String(1000))
-    pronounce = db.Column(db.String(1000))
-    learning =db.Column(db.String(10))
+    meaning = db.Column(db.String())
+    pronounce = db.Column(db.String())
+    learning =db.Column(db.String())
     section = db.Column(db.String())
 
 class Progress(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     answerrate = db.Column(db.String())
-    answered = db.Column(db.String(1000))
-    section = db.Column(db.String(100))
+    answered = db.Column(db.String())
+    section = db.Column(db.String())
