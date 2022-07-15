c5eb70aa6aca469d464c5d2a0f674f2b0130c0f3
goto <megumigoto65@gmail.com>
2022-07-15 16:53:54 +0900

    tango

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -12,12 +12,13 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'dsafhisdf'
-    # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or  'postgresql://postgres:4869@localhost:5432/tango1'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hyymjvfgumbpdt:23b9256aa30060cd645b103d65638329293fbde90f6260573693ea273edd7d98@ec2-54-152-28-9.compute-1.amazonaws.com:5432/d23ofadol35oc2'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or  'postgresql://postgres:4869@localhost:5432/tango1'
     # DATABASE_URL = os.environ['DATABASE_URL']
     # conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     migrate = Migrate(app, db)
     db.init_app(app)
+
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hyymjvfgumbpdt:23b9256aa30060cd645b103d65638329293fbde90f6260573693ea273edd7d98@ec2-54-152-28-9.compute-1.amazonaws.com:5432/d23ofadol35oc2'
 
 
 
